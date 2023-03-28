98b3fa6950496c1b41d366e04266c1d7319dc58f
Akshat Bhuhagal <akshatbhuhagal@gmail.com>
2023-03-28 20:03:09 +0530

    Initial Commit

---
+++ app/src/main/java/com/compose/memeoverpowered/model/MemeModel.kt
@@ -0,0 +1,13 @@
+package com.compose.memeoverpowered.model
+
+data class MemeModel(
+    val author: String,
+    val nsfw: Boolean,
+    val postLink: String,
+    val preview: List<String>,
+    val spoiler: Boolean,
+    val subreddit: String,
+    val title: String,
+    val ups: Int,
+    val url: String
+)
