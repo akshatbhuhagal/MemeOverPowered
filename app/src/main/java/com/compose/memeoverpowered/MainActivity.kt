835337550d4500ab67cf5839b44a1f2cbd586d0c
Akshat Bhuhagal <akshatbhuhagal@gmail.com>
2023-03-28 21:57:34 +0530

    API calling Commit

--- app/src/main/java/com/compose/memeoverpowered/MainActivity.kt
+++ app/src/main/java/com/compose/memeoverpowered/MainActivity.kt
@@ -1,43 +1,76 @@
 package com.compose.memeoverpowered
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
+import coil.compose.AsyncImage
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.compose.memeoverpowered.ui.theme.MemeOverPoweredTheme
+import org.json.JSONObject
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+
+            val scaffoldState = rememberScaffoldState()
+            val context = LocalContext.current
+            var memeUrl by remember { mutableStateOf("") }
+
             MemeOverPoweredTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
+                Scaffold(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    scaffoldState = scaffoldState
                 ) {
-                    Greeting("Android")
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center,
+                    ) {
+
+                        Button(onClick = {
+
+                            val url = "https://meme-api.com/gimme"
+
+                            val queue = Volley.newRequestQueue(context)
+
+                            val stringRequest = StringRequest(Request.Method.GET, url, { response ->
+
+                                val person = JSONObject(response)
+                                memeUrl = person.getString("url")
+
+                            }, {
+                                println("That didn't work!")
+                            })
+
+                            queue.add(stringRequest)
+
+                        }) {
+                            Text(text = "Get Meme")
+                        }
+
+                        AsyncImage(
+                            model = memeUrl,
+                            contentDescription = null
+                        )
+
+
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MemeOverPoweredTheme {
-        Greeting("Android")
     }
 }
