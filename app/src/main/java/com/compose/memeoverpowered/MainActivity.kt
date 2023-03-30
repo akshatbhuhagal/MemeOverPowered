6706c89723b5e1445d3358615a533d21b16465d3
Akshat Bhuhagal <akshatbhuhagal@gmail.com>
2023-03-30 19:52:42 +0530

    UI commit

--- app/src/main/java/com/compose/memeoverpowered/MainActivity.kt
+++ app/src/main/java/com/compose/memeoverpowered/MainActivity.kt
@@ -2,15 +2,22 @@ package com.compose.memeoverpowered
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material.icons.rounded.Call
+import androidx.compose.material.icons.rounded.Share
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.android.volley.Request
 import com.android.volley.toolbox.StringRequest
@@ -43,28 +50,58 @@ class MainActivity : ComponentActivity() {
                         AsyncImage(
                             model = memeUrl,
                             contentDescription = null,
-                            modifier = Modifier.fillMaxHeight(0.5f).fillMaxWidth(1f)
+                            modifier = Modifier
+                                .fillMaxHeight(0.5f)
+                                .fillMaxWidth(1f)
                         )
 
-                        Button(onClick = {
+                        Row(
+                            horizontalArrangement = Arrangement.Center,
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
 
-                            val url = "https://meme-api.com/gimme"
+                            Button(onClick = {
 
-                            val queue = Volley.newRequestQueue(context)
+                                val url = "https://meme-api.com/gimme"
 
-                            val stringRequest = StringRequest(Request.Method.GET, url, { response ->
+                                val queue = Volley.newRequestQueue(context)
 
-                                val person = JSONObject(response)
-                                memeUrl = person.getString("url")
+                                val stringRequest = StringRequest(Request.Method.GET, url, { response ->
 
-                            }, {
-                                println("That didn't work!")
-                            })
+                                    val person = JSONObject(response)
+                                    memeUrl = person.getString("url")
 
-                            queue.add(stringRequest)
+                                }, {
+                                    println("That didn't work!")
+                                })
 
-                        }) {
-                            Text(text = "Get Meme")
+                                queue.add(stringRequest)
+
+                            }) {
+                                Text(text = "Get Meme")
+                            }
+
+                            Box(Modifier.width(12.dp)) {
+
+                            }
+
+                            Button(onClick = {
+
+                                val sendIntent: Intent = Intent().apply {
+                                    action = Intent.ACTION_SEND
+                                    putExtra(Intent.EXTRA_TEXT, memeUrl)
+                                    type = "text/plain"
+                                }
+                                val shareIntent = Intent.createChooser(sendIntent, null)
+
+                                context.startActivity(shareIntent)
+
+                            }) {
+                                Icon(
+                                    Icons.Filled.Share, "Share Meme",
+                                    Modifier.size(14.dp)
+                                )
+                            }
                         }
 
 
